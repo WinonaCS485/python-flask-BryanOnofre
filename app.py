0254413e513a39851213f036690d1043cface770
BryanOnofre <onofreb128@gmail.com>
2020-02-02 20:43:13 -0600

    Python pep8 formatting sillyness

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from flask import Flask, render_template
 import pymysql.cursors
 
 # Connect to the database
@@ -15,45 +16,50 @@ output = ''
 try:
     with connection.cursor() as cursor:
         # Select all Students
-        sql = "SELECT * from Students WHERE fName =  " + "'" + userInput  + "'"
-        
+        sql = "SELECT * from Students WHERE fName =  " + "'" + userInput + "'"
+
         # execute the SQL command
         cursor.execute(sql)
-        
+
         # get the results
         for result in cursor:
             output = result
- 
+
         # If you INSERT, UPDATE or CREATE, the connection is not autocommit by default.
-        # So you must commit to save your changes. 
+        # So you must commit to save your changes.
         # connection.commit()
 
 finally:
     connection.close()
 
-from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/cakes')
 def cakes():
     return render_template('cakes.html')
 
+
 @app.route('/page')
 def page():
-     return render_template('page.html')
+    return render_template('page.html')
+
 
 @app.route('/database')
 def database():
-     return render_template('database.html', output = output )
+    return render_template('database.html', output=output)
+
 
 @app.route('/hello/<name>')
 def hello(name):
-    return render_template('page.html', name = name )
+    return render_template('page.html', name=name)
+
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port = 8643 )
+    app.run(debug=True, host='0.0.0.0', port=8643)
