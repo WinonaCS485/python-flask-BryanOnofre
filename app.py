719799602a0dad137435e32b93d5af55f1622784
BryanOnofre <onofreb128@gmail.com>
2020-02-02 19:14:47 -0600

    Add pymysql and connect to database

--- app.py
+++ app.py
@@ -1,3 +1,36 @@
+import pymysql.cursors
+
+# Connect to the database
+connection = pymysql.connect(host='mrbartucz.com',
+                             user='pi8643bb',
+                             password='sailboat128',
+                             db='pi8643bb_University',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+# userInput = 'Kami'
+userInput = input("Enter Name: ")
+output = ''
+
+try:
+    with connection.cursor() as cursor:
+        # Select all Students
+        sql = "SELECT * from Students WHERE fName =  " + "'" + userInput  + "'"
+        
+        # execute the SQL command
+        cursor.execute(sql)
+        
+        # get the results
+        for result in cursor:
+            output = result
+ 
+        # If you INSERT, UPDATE or CREATE, the connection is not autocommit by default.
+        # So you must commit to save your changes. 
+        # connection.commit()
+
+finally:
+    connection.close()
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
